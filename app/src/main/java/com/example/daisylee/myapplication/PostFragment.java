f921c75d5033f35d6d44e1dd182f923094457f2c
ml332034 <ml332034@gmail.com>
2018-06-02 11:44:49 +0800

    new added

--- app/src/main/java/com/example/daisylee/myapplication/PostFragment.java
+++ app/src/main/java/com/example/daisylee/myapplication/PostFragment.java
@@ -1,18 +1,38 @@
 package com.example.daisylee.myapplication;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PostFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_post, container, false);
-        return view;
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+
+
+
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_post, container, false);
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        appBarLayout = (AppBarLayout) rootView.findViewById(R.id.appbarid);
+        tabLayout = (TabLayout) rootView.findViewById(R.id.tablayout);
+        setupViewPager(viewPager);
+        return rootView;
+    }
+    private void setupViewPager(ViewPager PagerAdapter) {
+        PagerAdapter adapter = new PagerAdapter(getChildFragmentManager());
+        adapter.AddFragment(new schoolactivity(), "學校");
+        adapter.AddFragment(new tronclass(), "Tronclass");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
